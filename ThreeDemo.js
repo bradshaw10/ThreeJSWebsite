39a1bdc238f60f3b771d7661f01edc67b5c2cda6
bradshaw10 <eoghan.bradshaw@gmail.com>
2021-10-31 21:20:08 +0000

    updated project structure

--- ThreeDemo.js
+++ ThreeDemo.js
@@ -1,23 +1,3 @@
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-// Option 2: Import just the parts you need.
-import { Scene } from 'three';
-
-const scene = new Scene();
-const controls = new OrbitControls();
-
-if ( WEBGL.isWebGLAvailable() ) {
-
-	// Initiate function or other initializations here
-	animate();
-
-} else {
-
-	const warning = WEBGL.getWebGLErrorMessage();
-	document.getElementById( 'container' ).appendChild( warning );
-
-}
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -26,8 +6,9 @@ const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.inner
 camera.position.set( 0, 0, 100 );
 camera.lookAt( 0, 0, 0 );
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene(); 
 
+//create a blue LineBasicMaterial
 const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
 const points = [];
